d60c6627a9357af151e7e55ed3da4d8af1e5c2af
Marco Brotas <marco@mediamonks.com>
2019-06-06 13:17:23 +0200

    Fixes related to golint

--- handler.go
+++ handler.go
@@ -1,6 +1,6 @@
 package command
 
-// Handler must be implemented for a type to qualify as an command handler.
+// Handler must be implemented for a type to qualify as a command handler.
 type Handler interface {
 	Handle(cmd Command)
 }
